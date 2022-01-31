37502b2367da7e5dd539c4b67525a9e2edfbece1
DaliborA <d.arula@adriamedia.hr>
2022-01-31 14:03:07 +0100

    video mobile autoplay fix

--- _javascript/main.js
+++ _javascript/main.js
@@ -2,6 +2,33 @@
 var viewWidth = document.querySelector('body').offsetWidth;
 
 // Video
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('headerVideo', {
+    width: '100%',
+    height: '450',
+    videoId: 'U0muqHbaggg',
+    playerVars: { 'autoplay': 1, 'playsinline': 1 },
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.mute();
+  event.target.playVideo();
+}
 
 // Animation slika naslov on scroll
 gsap.from('#anim__print__head',{
